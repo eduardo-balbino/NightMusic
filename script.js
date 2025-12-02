983f7dcab0882de0004498afc4191e97a18a4fab
Eduardo Balbino <valencabalbino@gmail.com>
2025-12-02 01:29:53 -0300

    test: -add ambiente de testes -configurando botões (não finalizado) -organizando sintaxe do js -planejando uma estratégia melhor

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
+// ---------- DOM ----------
+const audioPlayer = document.getElementById("audio-player")
+const btnReturn = document.getElementById("return-button")
+const btnPlay = document.getElementById("play-button")
+const btnPause = document.getElementById("pause-button")
+const btnSkip = document.getElementById("skip-button")
 const input = document.getElementById("musicas")
 const playlist = []
 
-//Upload Músicas
+
+// ---------- Upload ----------
 input.addEventListener('change', (event) => {
     const arquivos = event.target.files
-
+    
     for (let arquivo of arquivos) {
        const musica = {
-        id: crypto.randomUUID,
-        name: arquivo.name,
-        url: URL.createObjectURL(arquivo)
-       } 
-       
-       playlist.push(musica)
+           id: crypto.randomUUID,
+           name: arquivo.name,
+           url: URL.createObjectURL(arquivo)
+        } 
+        
+        playlist.push(musica)
     }
     
-    //Salva no localStorage
+    audioPlayer.src = url
+
+    // Salvar
     localStorage.setItem('playlist', JSON.stringify(playlist))
     const playlists = JSON.parse(localStorage.getItem('playlist') || [])
-
-    console.log(playlist)
+    
+    console.log(playlists)
 })
 
+
+// ---------- Botões ----------
+btnPlay.addEventListener('click', () => {
+    audioPlayer.play()
+    alert('play')
+})
+
+btnPause.addEventListener('click', () => {
+    audioPlayer.pause()
+})
