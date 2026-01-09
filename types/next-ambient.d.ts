56c58a32628fefe53a07d84971967823ad3723b9
ossmoralus <ossmoralus@users.noreply.github.com>
2026-01-09 00:36:03 -0300

    chore(types): add minimal ambient declarations for next/* modules

---
+++ types/next-ambient.d.ts
@@ -0,0 +1,15 @@
+declare module 'next/app' {
+  export type AppProps = any;
+}
+declare module 'next/head' {
+  const Head: any;
+  export default Head;
+}
+declare module 'next/document' {
+  const Document: any;
+  export default Document;
+}
+declare module 'next/link' {
+  const Link: any;
+  export default Link;
+}
