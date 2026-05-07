697e7deac2ad5846df408f7687d2613b3a274c01
Eduardo Balbino <128527317+eduardo-balbino@users.noreply.github.com>
2026-05-07 19:26:21 -0300

    Refactor user routes into layers

---
+++ backend/controllers/users.controller.ts
@@ -0,0 +1,90 @@
+import { Request, Response, NextFunction } from "express";
+import {
+  getUserById,
+  InvalidCredentialsError,
+  listUsers,
+  loginUser,
+  LoginUserInput,
+  registerUser,
+  RegisterUserInput,
+  UserAlreadyExistsError,
+} from "../services/users.service.js";
+
+function handleKnownUserError(
+  error: unknown,
+  res: Response,
+  next: NextFunction,
+) {
+  if (
+    error instanceof UserAlreadyExistsError ||
+    error instanceof InvalidCredentialsError
+  ) {
+    return res.status(error.statusCode).json({
+      message: error.message,
+    });
+  }
+
+  return next(error);
+}
+
+export async function registerUserController(
+  req: Request<unknown, unknown, RegisterUserInput>,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const user = await registerUser(req.body);
+
+    return res.status(201).json({
+      message: "User created",
+      user,
+    });
+  } catch (error) {
+    return handleKnownUserError(error, res, next);
+  }
+}
+
+export async function loginUserController(
+  req: Request<unknown, unknown, LoginUserInput>,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const loginResult = await loginUser(req.body);
+
+    return res.json({
+      message: "Login successful",
+      ...loginResult,
+    });
+  } catch (error) {
+    return handleKnownUserError(error, res, next);
+  }
+}
+
+export async function listUsersController(
+  _req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const users = await listUsers();
+
+    return res.json({ users });
+  } catch (error) {
+    return next(error);
+  }
+}
+
+export async function getUserByIdController(
+  req: Request<{ id: string }>,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const user = await getUserById(req.params.id);
+
+    return res.json({ user });
+  } catch (error) {
+    return next(error);
+  }
+}
